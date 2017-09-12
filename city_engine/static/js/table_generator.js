d020063d7f2825076a2618f2f9063130bb34ce6f
Kleju0011 <michal.strumecki@wp.pl>
2017-09-12 19:57:36 +0200

    12-09-2017 19:57

--- city_engine/static/js/table_generator.js
+++ city_engine/static/js/table_generator.js
@@ -1,34 +1,10 @@
 $(document).ready(function() {
 
-//    $('.hexagon#1').mouseover(function(){
-//        $('.hexInfoDetail#hexBox1').show();
-//    }).mouseout(function(){
-//        $('.hexInfoDetail#hexBox1').hide();
-//        });
-//
-//    $('.hexagon#2').mouseover(function(){
-//        $('.hexInfoDetail#hexBox2').show();
-//    }).mouseout(function(){
-//        $('.hexInfoDetail#hexBox2').hide();
-//        });
-//
-//    $('.hexagon#3').mouseover(function(){
-//        $('.hexInfoDetail#hexBox3').show();
-//    }).mouseout(function(){
-//        $('.hexInfoDetail#hexBox3').hide();
-//        });
-//    $('hexagon#1').hover(function(){
-//        $('.hexInfoDetail#hexBox1').show();
-//    }, function(){
-//        $('.hexInfoDetail#hexBox1').hide();
-//    });
-    var text = $('.hexagon#1').attr('id');
-    $('#test').text(text);
-
     $('.hexagon').mouseover(function(){
         var id_text = $(this).attr('id');
         $('.hexInfoDetail#hexBox'+id_text+'').show();
     }).mouseout(function(){
+        var id_text = $(this).attr('id');
         $('.hexInfoDetail#hexBox'+id_text+'').hide();
         });
 
